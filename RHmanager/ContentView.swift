5c665702b852c32eba0d2ace959c5356b3bf2150
Alejandro <Alejandro.amb85@gmail.com>
2026-09-12 18:24:43 +0200

    Initial Commit

---
+++ RHmanager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RHmanager
+//
+//  Created by Alejandro Montero Barrios on 12/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RHmanager/RHmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RHmanagerApp.swift
+//  RHmanager
+//
+//  Created by Alejandro Montero Barrios on 12/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct RHmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
